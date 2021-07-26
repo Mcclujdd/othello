0e85a87ada93d3d11cc032412f3b9402b7d3d420
jason <mcclujdd@mail.com>
2021-07-26 19:14:36 -0400

    first commit

---
+++ venv/othello/check_valid_move.py
@@ -0,0 +1,11 @@
+from piece import pieces
+
+new_piece = Piece('a1', False)
+
+print(new_piece.value)
+
+# def verify_move(move_selection):
+#     adjacent_tiles = (-9, -8, -7, -1, 1, 7, 8, 9)
+#     if move_selection is in board_matrix_assignments:
+#         for tile in adjacent_tiles:
+#             if tile.is_black != playerB.is_black:
---
+++ venv/othello/piece/pieces.py
@@ -0,0 +1,8 @@
+
+
+class Piece:
+    def __init__(self, coordinates, is_black):
+       self.coordinates = coordinates
+       self.is_black = is_black
+
+    value = 'test'
